dfc5cfb0ed85b869b8c61f33455a2c876ed76fb9
Eamll <50913589+Eamll@users.noreply.github.com>
2022-08-05 12:17:02 -0400

    Added functionality for nav and language control

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var switchStatus = false;
 
 
 if (darkmode.inDarkMode && lightswitch == true) {
-
     darkmode.inDarkMode = false;
 }
 
@@ -15,38 +14,29 @@ $("#lightSwitch").on('change', function () {
     if ($(this).is(':checked')) {
         switchStatus = $(this).is(':checked');
         darkmode.inDarkMode = false;
-        // console.log(switchStatus);
     }
     else {
         switchStatus = $(this).is(':checked');
         darkmode.inDarkMode = true;
-        // console.log(switchStatus);
     }
 });
 
 //Load pages
 
-function isPillActive(id) {
-    var elem = $(`#${id}`);
-    if (elem.hasClass("nav-link active")) return true;
 
-    return false;
-}
 
 function replaceClass(id, oldClass, newClass) {
     var elem = $(`#${id}`);
 
     elem.removeClass(oldClass);
-
     elem.addClass(newClass);
 }
-
-
 
 function loadHome() {
     var lng = document.getElementById("langselector").value;
     replaceClass("nav_home", "nav-link", "nav-link active");
     replaceClass("nav_aboutme", "nav-link active", "nav-link");
+    replaceClass("nav_contactme", "nav-link active", "nav-link");
     switch (lng) {
         case "en":
             $("#container1").load("/pages/home/home_en.html");
@@ -63,6 +53,7 @@ function loadAboutme() {
     var lng = document.getElementById("langselector").value;
     replaceClass("nav_home", "nav-link active", "nav-link");
     replaceClass("nav_aboutme", "nav-link", "nav-link active");
+    replaceClass("nav_contactme", "nav-link active", "nav-link");
 
     switch (lng) {
         case "en":
@@ -73,39 +64,48 @@ function loadAboutme() {
             $("#container1").load("/pages/about-me/about-me-es.html");
             break;
     }
+}
 
+function loadContactMe() {
+    replaceClass("nav_home", "nav-link active", "nav-link");
+    replaceClass("nav_aboutme", "nav-link active", "nav-link");
+    replaceClass("nav_contactme", "nav-link", "nav-link active");
+    $("#container1").empty();
 }
 
 //Change language
 
+function isPillActive(id) {
+    var elem = $(`#${id}`);
+    if (elem.hasClass("nav-link active")) return true;
+
+    return false;
+}
+
 function loadlang() {
     var lng = document.getElementById("langselector").value;
-    var frase = document.getElementById("frase");
-
+    // var frase = document.getElementById("frase");
     switch (lng) {
         case "en":
-            // cnt.src = "/en.html";
-            frase.innerHTML = "Enjoys the art of code, hard work is my thing.";
+            $("#phrase").html("Enjoys the art of code, hard work is my thing.");
+            $("#contact_tag").html("Contact Me");
+            // frase.innerHTML = "Enjoys the art of code, hard work is my thing.";
             if (isPillActive("nav_home")) {
-                $("#container1").load("en.html");
+                $("#container1").load("/pages/home/home_en.html");
             } else if (isPillActive("nav_aboutme")) {
                 $("#container1").load("/pages/about-me/about-me-en.html");
             }
-
             break;
         case "es":
-            // cnt.src = "/es.html";
-            frase.innerHTML = "Disfruto el arte del codigo, el trabajo duro es lo mío.";
+            $("#phrase").html("Disfruto el arte del codigo, el trabajo duro es lo mío.");
+            $("#contact_tag").html("Contáctame");
+            // frase.innerHTML = "Disfruto el arte del codigo, el trabajo duro es lo mío.";
             if (isPillActive("nav_home")) {
-                $("#container1").load("es.html");
+                $("#container1").load("/pages/home/home_es.html");
             } else if (isPillActive("nav_aboutme")) {
                 $("#container1").load("/pages/about-me/about-me-es.html");
             }
-
-            // $("#container1").load("es.html");
             break;
     }
-
-
 }
 
